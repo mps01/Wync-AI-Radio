2ea6fb0be0b4af2131c5edc7b2d5f671a831a8ed
mps01 <madhavendra_bt2k19@dtu.ac.in>
2020-12-26 18:28:35 +0530

    Ui first phase completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_radio_ai/model/radio.dart';
@@ -49,9 +50,54 @@ class _HomePageState extends State<HomePage> {
             itemCount: radios.length,
             aspectRatio: 1.0,
             enlargeCenterPage: true,
+            onPageChanged: (index) {
+              //    _selectedRadio = radios[index];
+              final colorHex = radios[index].color;
+              //  _selectedColor = Color(int.tryParse(colorHex));
+              setState(() {});
+            },
             itemBuilder: (context, index) {
               final rad = radios[index];
-              return VxBox(child: ZStack([]))
+
+              return VxBox(
+                      child: ZStack(
+                [
+                  Positioned(
+                    top: 0.0,
+                    right: 0.0,
+                    child: VxBox(
+                      child: rad.category.text.uppercase.white.make().px16(),
+                    )
+                        .height(40)
+                        .black
+                        .alignCenter
+                        .withRounded(value: 10.0)
+                        .make(),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: VStack(
+                      [
+                        rad.name.text.xl3.white.bold.make(),
+                        5.heightBox,
+                        rad.tagline.text.sm.white.semiBold.make(),
+                      ],
+                      crossAlignment: CrossAxisAlignment.center,
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.center,
+                      child: [
+                        Icon(
+                          CupertinoIcons.play_circle,
+                          color: Colors.white,
+                        ),
+                        10.heightBox,
+                        "Double tap to play".text.gray300.make(),
+                      ].vStack())
+                ],
+              ))
+                  .clip(Clip.antiAlias)
                   .bgImage(
                     DecorationImage(
                         image: NetworkImage(rad.image),
@@ -62,12 +108,17 @@ class _HomePageState extends State<HomePage> {
                   .border(color: Colors.black, width: 5.0)
                   .withRounded(value: 60.0)
                   .make()
-                  .p16()
-                  .centered();
+                  .onInkDoubleTap(() {
+                //_playMusic(rad.url);
+              }).p16();
             },
-          )
+          ).centered(),
+          Align(
+            alignment: Alignment.bottomCenter,
+          ).pOnly(bottom: context.percentHeight * 12)
         ],
         fit: StackFit.expand,
+        clipBehavior: Clip.antiAlias,
       ),
     );
   }

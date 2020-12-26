cd7a3dc066b1c0ca9ebe41423634cba8e88715be
mps01 <madhavendra_bt2k19@dtu.ac.in>
2020-12-26 13:37:19 +0530

    Home Ui updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_radio_ai/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(MyApp());
 }
 
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_radio_ai/model/radio.dart';
 import 'package:flutter_radio_ai/utils/ai_util.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,10 +10,65 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<MyRadio> radios;
+  @override
+  void initState() {
+    super.initState();
+    fetchRadios();
+  }
+
+  fetchRadios() async {
+    final radioJson = await rootBundle.loadString("assets/radio.json");
+    radios = MyRadioList.fromJson(radioJson).radios;
+    print(radios);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: AIColors.primaryColor1,
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(LinearGradient(
+                colors: [AIColors.primaryColor1, AIColors.primaryColor2],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ))
+              .make(),
+          AppBar(
+            title: "Wync AI Radio".text.xl4.bold.white.make().shimmer(
+                primaryColor: Vx.purple300, secondaryColor: Colors.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+          ).h(100).p16(),
+          VxSwiper.builder(
+            itemCount: radios.length,
+            aspectRatio: 1.0,
+            enlargeCenterPage: true,
+            itemBuilder: (context, index) {
+              final rad = radios[index];
+              return VxBox(child: ZStack([]))
+                  .bgImage(
+                    DecorationImage(
+                        image: NetworkImage(rad.image),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.3), BlendMode.darken)),
+                  )
+                  .border(color: Colors.black, width: 5.0)
+                  .withRounded(value: 60.0)
+                  .make()
+                  .p16()
+                  .centered();
+            },
+          )
+        ],
+        fit: StackFit.expand,
+      ),
     );
   }
 }
